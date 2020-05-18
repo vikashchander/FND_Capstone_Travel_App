f649c41093464a56e8183f9481eef2f9e2dd85c4
vikash chander <vikashchander78384@gmail.com>
2020-05-19 01:02:44 +0530

    work on serviceWorker

--- src/client/index.js
+++ src/client/index.js
@@ -14,3 +14,16 @@ submitBtn.addEventListener('click',(event)=>{
       console.log(stateData.value,countryData.value,date.value)
       formHandler(stateData.value,countryData.value,date.value);
 })
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(registration => {
+        console.log("SW registered: ", registration);
+      })
+      .catch(registrationError => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
