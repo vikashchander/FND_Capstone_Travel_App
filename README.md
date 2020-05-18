1b91062f39140d97687a355541f44db7cac4726c
vikash chander <vikashchander78384@gmail.com>
2020-05-19 00:53:14 +0530

    edit readme.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# FEND Capstone - Travel App
+
+## Getting Started
+
+Remember that once you clone, you will still need to follows steps:
+
+- `npm install`
+- Add Environment Variables file .env
+### Signup for an API key
+ First, you will need to go
+  - Add  Weatherbit_API_key [here](https://www.weatherbit.io/account/create)
+  - Add Pixibay_Api_Key  [here](https://pixabay.com/api/docs/)
+  - Add Geonames_Api_Key [here](www.geonames.org/export/web-services.html)
+- Production command (npm run build-prod)  
+- Development command (npm run build-dev)  
+- test command (npm test)
+
+## Check This Project  Rubrics 
+
+you can check this app rubic:[Udacity's Rubrics](https://review.udacity.com/#!/rubrics/2669/view)
+  
+
+
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,7 +6,7 @@ export const formHandler = (stateValue, countryValue) => {
 };
 
 const postData = async (stateValue = "", countryValue = "") => {
-  const response = await fetch("/trip", {
+  const response = await fetch("/trips", {
     method: "POST",
     credentials: "same-origin",
     mode: "cors",
@@ -25,14 +25,11 @@ const postData = async (stateValue = "", countryValue = "") => {
 };
 
 function updateUI(data) {
-  console.log("vikash", data);
   const stateData = document.getElementById("state");
   const countryData = document.getElementById("country");
   const date = document.getElementById("date");
   var today = new Date();
-  var presentDate =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-  console.log(presentDate);
+  var presentDate =`${today.getFullYear()}-0${(today.getMonth() + 1)}-${today.getDate()}`;
   stateData.value = "";
   countryData.value = "";
   date.value = `${presentDate.toString()}`;
@@ -42,25 +39,21 @@ function updateUI(data) {
   cardInfo.className = " card-info";
   const imgTag = document.createElement("img");
   imgTag.className = " card-preview";
-  imgTag.setAttribute("src", data.imageURL.toString());
-  console.log(imgTag);
+  imgTag.setAttribute("src", data.imageURL);
   const tripData = document.createElement("h2");
   tripData.innerText = `My Trip : ${data.stateValue},${data.countryValue}`;
   const weatherInfo = document.createElement("h3");
   weatherInfo.innerText = "Typical Weather for then is :";
   const weatherData = document.createElement("h5");
-  weatherData.innerText = `Temperature : ${data.weatherInfo.temp}`;
+  weatherData.innerText = `Temperature : ${data.weatherInfo.temp} °C`;
   const weatherDesc = document.createElement("h6");
   weatherDesc.innerText = `${data.weatherInfo.weather.description}`;
   cardInfo.appendChild(tripData);
   cardInfo.appendChild(weatherInfo);
   cardInfo.appendChild(weatherData);
   cardInfo.appendChild(weatherDesc);
-  console.log(cardInfo.innerHTML);
   cardContainer.appendChild(imgTag);
   cardContainer.appendChild(cardInfo);
-  console.log(cardContainer);
   let mainContainer = document.getElementById("container");
-  console.log(mainContainer);
   mainContainer.appendChild(cardContainer);
 }
