4d32540478d8920420fa57979d90e4b5e7047938
vikash chander <vikashchander78384@gmail.com>
2020-05-19 23:09:04 +0530

    work on client testing

--- src/client/js/__test__/formHandler.test.js
+++ src/client/js/__test__/formHandler.test.js
@@ -1,5 +1,23 @@
+// import { updateUI } from "../formHandler";
+// test("test updateUI", () => {
+//   const data = {
+//     countryValue: "india",
+//     imageURL:
+//       "https://cdn.pixabay.com/photo/2016/07/08/00/26/elephant-1503503_150.png",
+//     stateValue: "delhi",
+//     weatherInfo: {
+//       city_name: "New Delhi",
+//       temp: 47.2,
+//       weather: {
+//         code: "800",
+//         description: "Clear sky",
+//         icon: "qwer",
+//       },
+//     },
+//   };
+//   expect(updateUI(data)).toContain(data.stateValue="delhi");
+// });
 
-
-  test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
-  });
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
